a59a680af6d568959029bafde13e420df0efba6f
leesuji <leesuji98@naver.com>
2025-04-11 17:22:47 +0900

    complete

--- Project24/baseball.cpp
+++ Project24/baseball.cpp
@@ -1,18 +1,70 @@
-#include "stdexcept"
-#include <string>
+#include <stdexcept>
+using namespace std;
 
-using std::string;
-using std::exception;
+struct GuessResult {
+    bool solved;
+    int strikes;
+    int balls;
+};
 
 class Baseball {
 public:
-	void guess(const string& guessNumber) {
-		if (guessNumber.length() != 3)
-			throw std::length_error("Must be three letters.");
+    explicit Baseball(const string& question)
+        : question(question) {
+    }
 
-		for (char ch : guessNumber) {
-			if (ch < '0' || ch > '9')
-				throw std::invalid_argument("Must be number");
-		}
-	}
+    bool isDuplicatedNumber(const string& guessNumber) {
+        return guessNumber[0] == guessNumber[1]
+            || guessNumber[0] == guessNumber[2]
+            || guessNumber[1] == guessNumber[2];
+    }
+
+    void assertIllegalArgument(const string& guessNumber) {
+        if (guessNumber.length() != 3) {
+            throw length_error("Must be three letters.");
+        }
+
+        for (char ch : guessNumber) {
+            if (ch >= '0' && ch <= '9') continue;
+            throw invalid_argument("Mus be number");
+        }
+
+        if (isDuplicatedNumber(guessNumber)) {
+            throw invalid_argument("Must not have the same number");
+        }
+    }
+
+    GuessResult guess(const string& guessNumber) {
+        assertIllegalArgument(guessNumber);
+
+        if (guessNumber == question) {
+            return { CorrectlyAnwser, 3, 0 };
+        }
+
+        int strikeCount = getStrikeCount(guessNumber);
+        int ballCount = getBallCount(guessNumber);
+        return { WrongAnswer, strikeCount, ballCount };
+    }
+
+    int getBallCount(const std::string& guessNumber) {
+        int result = 0;
+        for (int index = 0; index < 3; index++) {
+            if (guessNumber[index] == question[index]) continue;
+            if (question.find(guessNumber[index]) != string::npos) result++;
+        }
+        return result;
+    }
+
+    int getStrikeCount(const std::string& guessNumber) {
+        int strikeCount = 0;
+        for (int index = 0; index < 3; index++) {
+            if (guessNumber[index] == question[index]) strikeCount++;
+        }
+        return strikeCount;
+    }
+
+private:
+    string question;
+    static const bool CorrectlyAnwser = true;
+    static const bool WrongAnswer = false;
 };
--- Project24/baseball_test.cpp
+++ Project24/baseball_test.cpp
@@ -1,11 +1,12 @@
 #include "gmock/gmock.h"
 #include "baseball.cpp"
+#include <iostream>
 
 using namespace testing;
 
 class BaseballFixture : public Test {
 public:
-	Baseball game;
+	Baseball game{"123"};
 	void assertIllegalArgument(string guessNumber) {
 		try {
 			game.guess(guessNumber);
@@ -20,6 +21,16 @@ public:
 TEST_F(BaseballFixture, ThrowExceptionWhenInvalidCase) {
 	assertIllegalArgument("12");
 	assertIllegalArgument("12s");
+	assertIllegalArgument("121");
+}
+
+TEST_F(BaseballFixture, MatchedNumber) {
+	Baseball game("123");
+	GuessResult result = game.guess("456");
+
+	EXPECT_FALSE(result.solved);
+	EXPECT_EQ(0, result.strikes);
+	EXPECT_EQ(0, result.balls);
 }
 
 int main() {
