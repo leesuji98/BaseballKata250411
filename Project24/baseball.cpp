ed624961b2d0aee554ddf4be3a86973b708f24b6
leesuji <leesuji98@naver.com>
2025-04-11 16:18:56 +0900

    invalid argument

--- Project24/baseball.cpp
+++ Project24/baseball.cpp
@@ -9,5 +9,10 @@ public:
 	void guess(const string& string) {
 		if (string.length() != 3)
 			throw std::length_error("Must be three letters.");
+
+		for (char ch : string) {
+			if (ch < '0' || ch > '9')
+				throw std::invalid_argument("Must be number");
+		}
 	}
 };
--- Project24/baseball_test.cpp
+++ Project24/baseball_test.cpp
@@ -12,6 +12,11 @@ TEST(BaseballGame, ThrowExceptionWhenInputLengthIsUnmached) {
 	EXPECT_THROW(game.guess(string("12")), std::length_error);
 }
 
+TEST(BaseballGame, ThrowExceptionWhenInvalidChar) {
+	Baseball game;
+	EXPECT_THROW(game.guess(string("12s")), std::invalid_argument);
+}
+
 int main() {
 	::testing::InitGoogleMock();
 	return RUN_ALL_TESTS();
