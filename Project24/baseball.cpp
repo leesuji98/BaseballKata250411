b2b9b3d8e8ca6a8e8de7c6dc2952805ba7ceef21
leesuji <leesuji98@naver.com>
2025-04-11 16:14:46 +0900

    exception Check refactoring

--- Project24/baseball.cpp
+++ Project24/baseball.cpp
@@ -7,6 +7,7 @@ using std::exception;
 class Baseball {
 public:
 	void guess(const string& string) {
-		throw std::length_error("Must be three letters.");
+		if (string.length() != 3)
+			throw std::length_error("Must be three letters.");
 	}
 };
